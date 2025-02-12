9b666e8e70bf5c965f3b3b4e893a3ce9f5ed13d7
ITHUB\Student <qew>
2025-02-12 16:48:40 +0300

    CP_1
    Changed: Script Command - public void - protected void

--- My project (3)/Assets/Scripts/Command.cs
+++ My project (3)/Assets/Scripts/Command.cs
@@ -7,7 +7,7 @@ public abstract class Command
     public abstract void Invoke(Vector2 position);
 
 
-    public void LogPosition(Vector2 position)
+    protected void LogPosition(Vector2 position)
     {
         Debug.Log($"Command executed at position: {position}");
     }
