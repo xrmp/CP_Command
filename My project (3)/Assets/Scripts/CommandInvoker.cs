9d3ddceeebe9a837bb632377d65dda5351368ac5
ITHUB\Student <qew>
2025-02-12 17:34:41 +0300

    CP_2,3
    FixUpdate

--- My project (3)/Assets/Scripts/CommandInvoker.cs
+++ My project (3)/Assets/Scripts/CommandInvoker.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
-using static UnityEngine.Rendering.VirtualTexturing.Debugging;
 
 public class CommandInvoker : MonoBehaviour
 {
